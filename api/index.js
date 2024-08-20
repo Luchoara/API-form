bf262634572b7afa178a0bb4df02dfd3101db6a1
Luis Aragón <99699458+Luchoara@users.noreply.github.com>
2024-08-20 12:06:22 -0300

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_URL = "https://api.routingapi.com/rtbs.json?key=ab514634-6955-4f94-9e22-5de033366d2f"; // //MEDICARE 803  //  URL del API real
+
+app.post("/api/proxy", (req, res) => {
+  const { publisher_id, caller_number } = req.body;
+
+  const url = `${API_URL}&publisher_id=${publisher_id}&caller_number=${caller_number}`;
+
+  request.post(
+    {
+      url: url,
+      json: true,
+      body: req.body,
+    },
+    (error, response, body) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.status(response.statusCode).send(body);
+    }
+  );
+});
+
+module.exports = app; // Exportar la app para que Vercel la use
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running");
+});
---
+++ api/proxy.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const allowedOrigins = [
+	"http://localhost:3000", // Desarrollo local
+	"https://integration-forms.vercel.app/", // Dominio en Vercel
+];
+
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+	})
+);
+
+app.use(express.json());
