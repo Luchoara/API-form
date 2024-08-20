42578496f23c4b2a2681ed73fa2c08b30d96e765
Lucho <cheluiviajes@gmail.com>
2024-08-20 18:11:44 -0300

    index.js edited

--- api/index.js
+++ api/index.js
@@ -3,22 +3,12 @@ const request = require("request");
 const cors = require("cors");
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000", // Origin local para desarrollo
-  "https://tu-dominio-vercel.vercel.app" // Agrega aquí tu dominio de Vercel cuando esté desplegado
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: "POST",
-  allowedHeaders: ["Content-Type"],
-}));
-
+app.use(cors());
 app.use(express.json());
 
-const API_URL = "https://api.routingapi.com/rtbs.json?key=ab514634-6955-4f94-9e22-5de033366d2f"; 
+const API_URL = "https://api.routingapi.com/rtbs.json?key=ab514634-6955-4f94-9e22-5de033366d2f";
 
-app.post("/proxy", (req, res) => {
+app.post("/api/proxy", (req, res) => {
   const { publisher_id, caller_number } = req.body;
 
   const url = `${API_URL}&publisher_id=${publisher_id}&caller_number=${caller_number}`;
@@ -38,7 +28,4 @@ app.post("/proxy", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`);
-});
+module.exports = app;
