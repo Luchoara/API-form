13aba036918e0e70698de183311e60df052a626b
Aragon Luis <cheluiviajes@gmail.com>
2024-09-17 18:38:14 -0300

    edited for 816 - Medicare CPA

--- api/proxy.js
+++ api/proxy.js
@@ -1,55 +1,67 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/proxy', async (req, res) => {
-  const { 
-    publisher_id,
-    caller_number,
-    first_name,
-    last_name,
-    email,
-    state,
-    caller_zip,
-    trusted_form_cert_url,
-  } = req.body;
-  
-  try {
-    const baseURL = "https://retreaverdata.com/data_writing";
-    const params = new URLSearchParams({
-      key: "cfaa884a-044b-4e93-9bbb-c5a51295cb3e", // 816 Medicare CPA data_writing
-      publisher_id,
-      caller_number,
-      first_name,
-      last_name,
-      email,
-      state,
-      caller_zip,
-      trusted_form_cert_url,
-    });
+app.post("/api/proxy", async (req, res) => {
+	const {
+		caller_number,
+		first_name,
+		last_name,
+		email,
+		dob,
+		address,
+		city,
+		state,
+		caller_zip,
+		traffic_source_id,
+		source_url,
+		original_lead_submit_date,
+		trusted_form_cert_url,
+	} = req.body;
 
-    const fullURL = `${baseURL}?${params.toString()}`;
-    console.log("Full URL:", fullURL);
+	try {
+		const baseURL = "https://retreaverdata.com/data_writing";
+		const params = new URLSearchParams({
+			key: "cfaa884a-044b-4e93-9bbb-c5a51295cb3e", // 816 Medicare CPA data_writing
+			caller_number,
+			first_name,
+			last_name,
+			email,
+			dob,
+			address,
+			city,
+			state,
+			caller_zip,
+			traffic_source_id,
+			source_url,
+			original_lead_submit_date,
+			trusted_form_cert_url,
+		});
 
-    const response = await fetch(fullURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+		const fullURL = `${baseURL}?${params.toString()}`;
+		console.log("Full URL:", fullURL);
 
-    if (response.ok) {
-      const data = await response.json();
-      res.status(200).json({ data, fullURL });
-    } else {
-      res.status(response.status).json({ message: "Error in API response" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error", error: error.message });
-  }
+		const response = await fetch(fullURL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			res.status(200).json({ data, fullURL });
+		} else {
+			res.status(response.status).json({ message: "Error in API response" });
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: "Internal server error", error: error.message });
+	}
 });
 
 module.exports = app;
