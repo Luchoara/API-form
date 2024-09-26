5dec318566539eb9b4794e2c0010ce5ca70c647e
Aragon Luis <cheluiviajes@gmail.com>
2024-09-26 17:36:15 -0300

    fixed

--- api/proxy.js
+++ api/proxy.js
@@ -1,70 +1,64 @@
-import express from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch'; // Ahora utilizas import en vez de require
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch"; // Ahora utilizas import en vez de require
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/api/proxy", async (req, res) => {
-  const {
-    publisher_id,
-    caller_number,
-    first_name,
-    last_name,
-    email,
-    caller_state,
-    caller_zip,
-    attorney,
-    incident_date,
-    injured,
-    trusted_form_cert_url,
-  } = req.body;
+	const {
+		publisher_id,
+		caller_number,
+		first_name,
+		last_name,
+		email,
+		caller_state,
+		caller_zip,
+		attorney,
+		incident_date,
+		injured,
+		trusted_form_cert_url,
+	} = req.body;
 
-  try {
-    const baseURL = "https://rtb.retreaver.com/rtbs.json";
+	try {
+		const baseURL = "https://rtb.retreaver.com/rtbs.json";
 		const params = new URLSearchParams({
 			key: "136b19e3-3912-476a-8b5b-9a8de3fee354", // Campaign 818 MVA 1 - Pub 128
 			publisher_id,
-			
+			caller_number,
+			first_name,
+			last_name,
+			email,
+			caller_state,
+			caller_zip,
+			attorney,
+			incident_date,
+			injured,
+			trusted_form_cert_url,
 		});
 
 		const fullURL = `${baseURL}?${params.toString()}`;
 		console.log("Full URL:", fullURL);
 
-    const bodyData = {
-			caller_number,
-      first_name,
-      last_name,
-      email,
-      caller_state,
-      caller_zip,
-      attorney,
-      incident_date,
-      injured,
-      trusted_form_cert_url,
-    };
+		const response = await fetch(fullURL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-    const response = await fetch(fullURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bodyData),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      res.status(200).json({ data, fullURL });
-    } else {
-      const errorText = await response.text();
-      console.error("Error in API response:", errorText);
-      res.status(response.status).json({ message: "Error in API response", error: errorText });
-    }
-  } catch (error) {
-    console.error("Internal server error:", error);
-    res.status(500).json({ message: "Internal server error", error: error.message });
-  }
+		if (response.ok) {
+			const data = await response.json();
+			res.status(200).json({ data, fullURL });
+		} else {
+			res.status(response.status).json({ message: "Error in API response" });
+		}
+	} catch (error) {
+		res
+			.status(500)
+			.json({ message: "Internal server error", error: error.message });
+	}
 });
 
 export default app;
